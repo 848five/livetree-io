278d0fbe85349314001f4c3d85853308bbaefdb7
aramik <aramik@whalerockindustries.com>
2014-07-29 20:34:08 -0700

    added swig templates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var swig = require('swig');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -11,8 +12,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('html',swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', routes);
 app.use('/users', users);
+swig.setDefaults({cache:false});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
