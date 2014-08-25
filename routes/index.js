acce567ced8873af9836ddeb58a3d16c0206436d
aramik <aramik@whalerockindustries.com>
2014-08-25 15:46:22 -0700

    stage before removing ember and starting fresh, simple and clean

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var io = require('socket.io');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	 console.log('index.js -----------');
+	 res.render('index');
+	
 });
 
 module.exports = router;
